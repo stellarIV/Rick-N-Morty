7213ba9ad47befc6cda713054608fa169a565e39
stellarIV <152255130+stellarIV@users.noreply.github.com>
2024-06-14 18:01:52 +0300

    Update AboutView.vue
    2bd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -41,37 +41,51 @@ const CHARACTERS_QUERY = gql`
 
 
 <template>
-  <div id="para" class="bg-gradient-to-tr from-orange-500 to-slate-800 text-white pt-3 h-full">
+  <div id="para" class="bg-gradient-to-tr from-orange-500 to-slate-800 text-white pt-3 min-h-screen">
     <home />
-    <div v-if="error">Something went wrong...</div>
-    <div v-if="loading" class="h-[645px] flex justify-center items-center w-full"><img src="https://img.icons8.com/ios-glyphs/30/FFFFFF/loading-sign.png" alt="loading" class="animate-pulse duration-200"/></div>
-    <div v-else> 
-    <!-- v-for="episode in result.episode" :key="episode.id" class=" justify-evenly bg-gradient-to-br from-orange-600 to-slate-800 text-white w-full overflow-hidden rounded-3xl px-4 mb-2"> -->
-
-    
-    <div class="text-3xl flex justify-center hover:underline py-5">{{result.episode.episode}}- {{ result.episode.name }}<br></div>
-    <div class="text-xl flex justify-center">Creation time- {{ result.episode.created }}<br></div>
-    <div class="flex justify-center text-xl">Air Date- {{ result.episode.air_date }}<br></div>
-    <br><br>
-    
-  
-    <div class="text-3xl flex justify-center text-blue-400 pb-5">Characters in this episode</div>
-    <div class="grid grid-cols-3">
-    <div v-for="char in result.episode.characters" :key="char.id" class=" bg-[#271cfc32] w-[440px] rounded-3xl ml-5 my-4 overflow-hidden" >
-      <div class="flex w-[400px] items-center">
-
-        <img :src="char.image" class=" w-[200px] h-[200px] mr-10">
-        Name- {{ char.name }}<br/>
-        Status- {{ char.status }}<br/>
-        Species- {{ char.species }}<br/>
-        Gender- {{ char.gender }}
+    <div v-if="error" class="text-center text-xl py-10">Something went wrong...</div>
+    <div v-if="loading" class="h-[645px] flex justify-center items-center w-full">
+      <img
+        src="https://img.icons8.com/ios-glyphs/30/FFFFFF/loading-sign.png"
+        alt="loading"
+        class="animate-pulse duration-200"
+      />
+    </div>
+    <div v-else>
+      <div class="text-3xl flex justify-center hover:underline py-5 px-4 text-center">
+        {{ result.episode.episode }} - {{ result.episode.name }}
       </div>
-      
-    </div>
-    </div>
+      <div class="text-xl flex justify-center px-4 text-center">
+        Creation time - {{ result.episode.created }}
+      </div>
+      <div class="flex justify-center text-xl px-4 text-center">
+        Air Date - {{ result.episode.air_date }}
+      </div>
+      <br /><br />
 
- 
-  </div>
+      <div class="text-3xl flex justify-center text-blue-400 pb-5 px-4 text-center">
+        Characters in this episode
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-4">
+        <div
+          v-for="char in result.episode.characters"
+          :key="char.id"
+          class="bg-[#271cfc32] w-full rounded-3xl overflow-hidden p-5 flex flex-col items-center md:flex-row"
+        >
+          <img
+            :src="char.image"
+            class="w-full h-auto md:w-[200px] md:h-[200px] mb-4 md:mb-0 md:mr-5 rounded-full"
+            alt="Character Image"
+          />
+          <div class="text-center md:text-left">
+            <div class="font-bold">Name: {{ char.name }}</div>
+            <div>Status: {{ char.status }}</div>
+            <div>Species: {{ char.species }}</div>
+            <div>Gender: {{ char.gender }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
